6edc8d5f29c7143524892384e248dc568172593b
riantkb <kubota.rian@gmail.com>
2020-05-26 09:02:57 +0900

    fix

--- script.js
+++ script.js
@@ -27,10 +27,7 @@ function get_sample() {
     request.open("GET", url, true);
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                return request.responseText;
-            }
+            return request.responseText;
         }
     }
     request.send(null);
